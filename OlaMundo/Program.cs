bf8dcd7c7f33bdeae2e07ddec37cf8c1da6f8e70
Joao Gabriel <jlucenasilva1910@gmail.com>
2025-08-20 20:39:29 -0300

    fix: converter OlaMundo de Submódulo para pasta normal

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+
+
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");
+
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Olá {nome}, você tem {idade} anos.");      
+    }
+}
