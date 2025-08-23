c61854f2a99c205eaa647c60f0eb4daa2dd399a5
Joao Gabriel <jlucenasilva1910@gmail.com>
2025-08-22 23:33:18 -0300

    Mais um teste, antes do projeto automatizado

---
+++ src/OlaUsuario3/Program.cs
@@ -0,0 +1,14 @@
+﻿string Perguntar(string mensagem)
+{
+    Console.Write(mensagem);
+    return Console.ReadLine() ?? "";
+}
+
+void ExibirMensagem(string nome, string idade)
+{
+    Console.WriteLine($"Olá {nome}, você tem {idade} anos.");
+}
+
+var nome = Perguntar("Digite seu nome: ");
+var idade = Perguntar("Digite sua idade: ");
+ExibirMensagem(nome, idade);
